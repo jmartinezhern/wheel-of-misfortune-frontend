8b1ce97286bc79fa3f196e36dc19338ac627d92e
Jorge Martinez Hernandez <me@jmartinezhern.com>
2020-10-18 19:53:36 -0500

    Simplify state management in scenario and completion page
    Signed-off-by: Jorge Martinez Hernandez <me@jmartinezhern.com>

--- src/cljs/wheel_of_misfortune/components/complete.cljs
+++ src/cljs/wheel_of_misfortune/components/complete.cljs
@@ -19,7 +19,7 @@
      [:h3.step-title title]
      [:p.step-description description]]))
 
-(defn update-dist-of-neighbor
+(defn- update-dists
   [node
    neighbor
    dist-from-source
@@ -27,62 +27,43 @@
    current-dist-in-path]
   (let [dist-through-node (+ dist-from-source dist-from-node)]
     (when (or  (nil? current-dist-in-path)
-               (and  (not (nil? current-dist-in-path))
-                     (< dist-through-node current-dist-in-path)))
+               (< dist-through-node current-dist-in-path))
       {neighbor {:dist dist-through-node :prev node}})))
 
-(defn process-node
-  [graph
-   current-node
-   path]
-  (let [neighbors        (get graph current-node)
-        dist-from-source (if-let [curnode-in-path (get path current-node)]
-                           (get curnode-in-path :dist)
-                           0)
-        ;; This is the source node, so we'll give it 0 as distance 
-        new-path         (->> neighbors ;; for each of its neighbors
-                              (map (fn [[neighbor dist-to-cur-node]]
-                                     (update-dist-of-neighbor current-node
-                                                              neighbor
-                                                              dist-from-source
-                                                              dist-to-cur-node
-                                                              (get (get path neighbor) :dist))))
-                      ;; we'll compute the new distance and previous node
-                      ;; using the utility function (update-dist-of-neighbor)
-                              (into path))]
-    ;; which we'll return as new-path 
+(defn- process-node [graph node path]
+  (let [neighbors   (get graph node)
+        source-dist (if-let [node-in-path (get path node)]
+                      (get node-in-path :dist)
+                      0)
+        new-path    (->>
+                     neighbors
+                     (map (fn [[neighbor dist-to-cur-node]]
+                            (update-dists node
+                                          neighbor
+                                          source-dist
+                                          dist-to-cur-node
+                                          (get (get path neighbor) :dist))))
+                     (into path))]
     new-path))
 
-(defn dijkstra
-  [graph]
-  (loop [remaining-vertices (keys graph)
-         path               {}]
-    (if (seq remaining-vertices)
-      ;; for every node we process its neighbors
-      ;; using process-node
-      (recur (rest remaining-vertices)
-             (process-node graph (first remaining-vertices) path))
+(defn- dijkstra [graph]
+  (loop [remaining (keys graph)
+         path      {}]
+    (if (seq remaining)
+      (recur (rest remaining)
+             (process-node graph (first remaining) path))
       path)))
 
-(defn- optimal-solution
-  [graph source target]
-  (let [dijkstra-shortest-paths (dijkstra graph)]
-    ;; we compute the shortest paths 
-    (loop [new-target (get dijkstra-shortest-paths target)
-           ;; We begin from target
+(defn- optimal-solution [graph source target]
+  (let [shortest-paths (dijkstra graph)]
+    (loop [new-target (get shortest-paths target)
            path       [target]]
-      (let [next-new-target (get new-target :prev)]
-        ;; and get back to the previous node
-        (cond (nil? next-new-target)     :error
-              ;; we did not find a :prev reference
-              (= next-new-target source) (into [source] path)
-              ;; we are done,
-              ;; we return the path
-              :else                      (recur (get dijkstra-shortest-paths
-                                                     next-new-target)
-                           ;; we recur using the previous node
-                           ;; and adding it to the result path
-                                                (into [next-new-target] path)))))))
+      (let [next-target (get new-target :prev)]
+        (cond (nil? next-target)     :error
+              (= next-target source) (into [source] path)
+              :else                  (recur (get shortest-paths
+                                                 next-target)
+                                            (into [next-target] path)))))))
 
 (defn- state-scores [states]
   (into
@@ -110,10 +91,11 @@
                 states
                 termination]} (local-storage->edn scenario-id)
         transitions           (local-storage->edn (str scenario-id "-transitions"))
-        remaining-steps       (when (not won?)
+        remaining             (if (not (won?))
                                 (-> (state-scores states)
                                     (optimal-solution (last transitions) termination)
-                                    rest))]
+                                    rest)
+                                [])]
     (fn []
       [:span.main
        [:h1 (if won? "Crisis averted!" "Not quite")]
@@ -122,4 +104,4 @@
         [:h3#scenario-title {:style {:margin-bottom 0}} name]
         [:div
          (transitions->steps states transitions green)
-         (transitions->steps states remaining-steps yellow)]]])))
+         (transitions->steps states remaining yellow)]]])))
--- src/cljs/wheel_of_misfortune/components/scenario.cljs
+++ src/cljs/wheel_of_misfortune/components/scenario.cljs
@@ -10,26 +10,12 @@
 (defonce ^:private history (r/atom ""))
 (defonce ^:private scenario (r/atom {}))
 (defonce ^:private state (r/atom nil))
-(defonce ^:private won (r/atom false))
 (defonce ^:private transitions (r/atom []))
-
-(defn- navigate-to-complete-page [scenario-id]
-  (accountant/navigate!
-   (str "/scenarios/"
-        (scenario-id)
-        "/complete?won="
-        (if @won "true" "false"))))
-
-(defn- storage-transition-key [id]
-  (str id "-transitions"))
-
-(defn- scenario-id []
-  (get-in (session/get :route) [:route-params :scenario-id]))
 
 (defn- load-scenario [id]
   (go
     (let
-     [{:keys [states start] :as body} (<! (c/fetch-scenario id))]
+        [{:keys [states start] :as body} (<! (c/fetch-scenario id))]
       (reset! transitions [start])
       (reset! state start)
       (reset! scenario body)
@@ -48,8 +34,8 @@
       (= (:termination @scenario) t) (str
                                       output
                                       "\n\nScenario complete! Enter 'exit' to continue.")
-      (nil? next) "Invalid command"
-      :else output)))
+      (nil? next)                    "Invalid command"
+      :else                          output)))
 
 (defn- command-history []
   (fn []
@@ -61,12 +47,22 @@
      {:value    @history
       :readOnly true}]))
 
+(defn- complete-game! [won?]
+  (let [scenario-id (:id @scenario)
+        storage-key (str scenario-id "-transitions")]
+    (-> (.-localStorage js/window)
+        (.setItem storage-key @transitions))
+    (accountant/navigate!
+     (str "/scenarios/"
+          scenario-id
+          "/complete?won="
+          (if won? "true" "false")))))
+
 (defn- next-game-state! [cmd t]
   (cond
-    (= cmd "exit") (navigate-to-complete-page scenario-id)
-    (not (nil? t)) (reset! state t))
-  (when (= (:terminiation @scenario) t)
-    (reset! won true)))
+    (= cmd "exit") (complete-game! (= (:termination @scenario) t))
+    (nil? t)       (swap! transitions conj t)
+    (not (nil? t)) (reset! state t)))
 
 (defn command-line []
   (let [command (r/atom "")]
@@ -76,29 +72,23 @@
         :value     (str "> " @command)
         :on-change #(reset! command (subs (-> % .-target .-value) 2))
         :on-key-up #(when (= (.-which %) 13)
-                      (let [t      (transition @command (get-in
-                                                         @scenario
-                                                         [:states @state]))]
+                      (let [t (transition @command (get-in
+                                                    @scenario
+                                                    [:states @state]))]
                         (next-game-state! @command t)
-                        (swap! transitions conj t)
                         (reset! history (str @history
                                              "\n"
                                              (str "> " @command)
                                              "\n"
                                              (cmd-output t)))
-
                         (reset! command "")))}])))
 
 (def command-edit (with-meta command-line
                     {:component-did-mount #(.focus (rdom/dom-node %))}))
 
 (defn page []
-  (let [scenario-id                              (scenario-id)]
+  (let [scenario-id (get-in (session/get :route) [:route-params :scenario-id])]
     (load-scenario scenario-id)
-    (add-watch transitions :state-transition
-               (fn [_ _ new _]
-                 (-> (.-localStorage js/window)
-                     (.setItem (storage-transition-key scenario-id) new))))
     (fn []
       [:span
        [:h2#scenario-title (:name @scenario)]
